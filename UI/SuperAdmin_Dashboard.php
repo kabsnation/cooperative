5fb4d528f3e10e64d305df6bf950e11df5fcaf69
Dherp Cuevas <34502161+dherpcuevas@users.noreply.github.com>
2018-02-03 00:47:32 +0800

    Dashboard select date

--- UI/SuperAdmin_Dashboard.php
+++ UI/SuperAdmin_Dashboard.php
@@ -19,6 +19,17 @@ include('../UI/header/header_sadmin.php');
 							<h4> <span class="text-semibold">Welcome!</span> - Administrator </h4>
 						</div>
 
+
+						<div class="heading-elements">
+							<div class="content-group-lg">
+								<h6 style="float: left; margin-right: 10px;">Select Date:</h6>
+								<div class="input-group">
+									<input id="date_picker" type="text" class="form-control pickadate">
+								</div>
+							</div>
+							
+						</div>
+
 					</div>
 				</div>
 				<!-- /page header -->
@@ -97,13 +108,7 @@ include('../UI/header/header_sadmin.php');
 									<h6 class="panel-title text-semibold">Document Tracker</h6>
 
 									<div class="heading-elements">
-										<div class="content-group-lg">
-											<h6 style="float: left; margin-right: 10px; font-size: 12px;">Select Date:</h6>
-											<div class="input-group">
-												<span class="input-group-addon"><i class="icon-calendar"></i></span>
-												<input id="date_picker" type="text" name="date" value="<?php echo date('m/d/y');?>" class="form-control daterange-single">
-											</div>
-										</div>
+										
 									</div>
 								</div>
 
@@ -201,7 +206,18 @@ include('../UI/header/header_sadmin.php');
 		});
 	}
 
-	
+	function getDate(){
+		var currentDate = new Date();
+
+		var date = currentDate.getDate();
+		var month = currentDate.getMonth(); //Be careful! January is 0 not 1
+		var year = currentDate.getFullYear();
+		var dateString = date + " " +(month + 1) + "," + year;
+
+		document.getElementById("date_picker").value = currentDate;
+	}
+
+	window.onload = getDate;
 </script>
 </body>
 </html>
