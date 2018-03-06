f0d86c0c6a69b8cdefbacfffea3a7b7fe893b8a4
Dherp Cuevas <34502161+dherpcuevas@users.noreply.github.com>
2018-03-06 11:37:07 +0800

    Send Document New Design

--- UI/COOP_AddDocument.php
+++ UI/COOP_AddDocument.php
@@ -18,80 +18,84 @@ $departmentProfile = $account->getDepartmentAccounts($id);
 include('../UI/header/header_user.php');
 ?>
 
-                  <div class="content-wrapper">
-                    <form id="form1" action="documentFunction.php" method="POST" class="form-validate-jquery" enctype="multipart/form-data">
-                    <div class="content-wrapper">
-                        <div class="content">
+            
+                
+                <!-- Main Content -->
+                <div class="content-wrapper">
 
-                            <div class="panel panel-white">
-                                <div class="panel-heading">
-                                    <div class="panel-title">
-                                        <h3 class="panel-title">Send Document</h3>
-                                    </div>
+                    <!-- Content area -->
+                    <div class="content">
 
-                                    <div class="heading-elements">
-                                        <div class="heading-btn-group">
-                                        </div>
-                                    </div>
+                    <div class="panel panel-white">
+
+                        <form id="form1" action="documentFunction.php" method="POST" class="form-validate-jquery" enctype="multipart/form-data">
+                            <div class="panel-heading">
+                                <div class="panel-title">
+                                    <h3 class="panel-title">Send Document</h3>
                                 </div>
 
-                                <div class="panel-body">
+                                <div class="heading-elements">
+                                    <div class="heading-btn-group">
+                                    </div>
+                                </div>
+                            </div>
 
-                                    <fieldset class="content-group">
-                                        <div class="col-lg-12">
+                            <div class="panel-body">
+                                <fieldset class="content-group">
+                                    <div class="col-lg-12">
 
-                                            <div class="row">
-                                                
-                                                <div class="col-lg-6">
-                                                    <label class="control-label text-bold">Tracking Number:</label>
-                                                    <br/>
-                                                    <label id="trackingNumber" class="label" style="color: #000; font-size: 15px;">  <?php echo $trackingNumber;?></label>
-                                                        <input type="hidden" name="trackingNumber" value="<?php echo $trackingNumber;?>">
+                                        <div class="row">
+                                            
+                                            <div class="col-lg-6">
+                                                <label class="control-label text-bold">Tracking Number:</label>
+                                                <br/>
+                                                <label id="trackingNumber" class="label" style="color: #000; font-size: 15px;">  <?php echo $trackingNumber;?></label>
+                                                    <input type="hidden" name="trackingNumber" value="<?php echo $trackingNumber;?>">
+                                                </label>
+                                            </div>
+
+
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label class="control-label"><strong>From:</strong> </label>
+                                                    <?php if($adminAccount){
+                                                        foreach($adminAccount as $admin){?>
+                                                        <br>
+                                                    <label class="label" style="color: #000; font-size: 15px;" ><?php echo $admin['name'];?>
+                                                        <!-- echo yung session -->
+                                                        <input type="hidden" name="accountId" value="<?php echo "$id";?>">
                                                     </label>
-                                                </div>
+                                                    <?php }}?>
+                                              </div>
+                                            </div>
+                                        </div>
 
-
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label class="control-label"><strong>From:</strong> </label>
-                                                        <?php if($adminAccount){
-                                                            foreach($adminAccount as $admin){?>
-                                                            <br>
-                                                        <label class="label" style="color: #000; font-size: 15px;" ><?php echo $admin['name'];?>
-                                                            <!-- echo yung session -->
-                                                            <input type="hidden" name="accountId" value="<?php echo "$id";?>">
-                                                        </label>
-                                                        <?php }}?>
-                                                  </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label class="control-label"> <span class="text-danger">* </span> <strong> Document Title:</strong></label>
+                                                    <textarea type="text" name="title" id="txtDocumentName" class="form-control" required="required" minlength="1" maxlength="100"></textarea>
                                                 </div>
                                             </div>
 
-                                            <div class="row">
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label class="control-label"> <span class="text-danger">* </span> <strong> Document Title:</strong></label>
-                                                        <textarea type="text" name="title" id="txtDocumentName" class="form-control" required="required" minlength="1" maxlength="100"></textarea>
-                                                    </div>
-                                                </div>
-
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label class="control-label">Date Added:</label>
-                                                        <input type="text"  class="form-control" ID="dateTime" type="DateTime" readonly="true" value="<?php echo date("m/d/Y") ?>"></input>
-                                                        <input type="hidden" name="datetime" value="date('m/d/Y - h:m:s')">
-                                                    </div>
-                                                </div>
-
-                                            </div>
-
-                                            <div class="row">
-                                                <div class="col-lg-12">
-                                                    <div class="form-group">
-                                                        <label class="control-label"><strong>Message:</strong></label>
-                                                        <textarea type="text" class="summernote" id="message" name="message"></textarea>
-                                                    </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label class="control-label">Date Added:</label>
+                                                    <input type="text"  class="form-control" ID="dateTime" type="DateTime" readonly="true" value="<?php echo date("m/d/Y") ?>"></input>
+                                                    <input type="hidden" name="datetime" value="date('m/d/Y - h:m:s')">
                                                 </div>
                                             </div>
+
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <div class="form-group">
+                                                    <label class="control-label"><strong>Message:</strong></label>
+                                                    <textarea type="text" class="summernote" id="message" name="message"></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
 
                                         <div class="row">
                                             
@@ -136,71 +140,72 @@ include('../UI/header/header_user.php');
                                                 </div>
                                             </div>
 
-                                        <div class="col-lg-12">
-                                            <div class="col-md-12">
+                                            <div class="col-lg-12">
                                                 <div class="form-group">
                                                     <label><span class="text-danger">* </span><strong>Choose Recipients:</strong></label>
 
-                                        <input type="text" id="checker" class="label" disabled="true" required="required" >
-                                                  <table class="table datatable-html" id="table" style="font-size: 13px; width: 100%;">
-                                                            <thead>
-                                                                <tr>
-                                                                    <th style="width: 5%;"><input type="checkbox" class="styled" id="select-all"  name="select-all" onchange="addToHidden(this)" ></th>
-                                                                    <th style="width: 30%;">Recipients</th>
-                                                                    <th style="width: 20%;">Email</th>
-                                                                    <th style="width: 20%;">Type</th>
-                                                                </tr>
-                                                            </thead>
-                                                            <tbody>
-                                                                <?php if($cooperativeProfile){
-                                                                    foreach($cooperativeProfile as $coop){?>
-                                                                <tr>
-                                                                    <td><input type="checkbox"  name="checkbox[]" onchange="addToHidden(this)" value="<?php echo $coop['idAccounts'];?>"></td>
-                                                                     <td><?php echo $coop['Cooperative_Name'];?></td>
-                                                                     <td><?php echo $coop['Email_Address'];?></td>
-                                                                     <td>Cooperative</td>
-                                                                </tr>
-                                                                <?php }}?>
-                                                                <?php if($departmentProfile){
-                                                                    foreach($departmentProfile as $dept){?>
-                                                                <tr>
-                                                                    <td><input type="checkbox" name="checkbox[]" value="<?php echo $dept['idAccounts'];?>" onchange="addToHidden(this)"></td>
-                                                                     <td><?php echo $dept['Department'];?></td>
-                                                                     <td><?php echo $dept['Email_Address'];?></td>
-                                                                     <td>Department</td>
-                                                                </tr>
-                                                                <?php }}?>
-                                                            </tbody>
-                                                        </table>
+                                                    <input type="text" id="checker" class="label" disabled="true" required="required" >
+                                                    <table class="table datatable-html" id="table" style="font-size: 13px; width: 100%;">
+                                                        <thead>
+                                                            <tr>
+                                                                <th style="width: 5%;"><input type="checkbox" class="styled" id="select-all"  name="select-all" onchange="addToHidden(this)" ></th>
+                                                                <th style="width: 30%;">Recipients</th>
+                                                                <th style="width: 20%;">Email</th>
+                                                                <th style="width: 20%;">Type</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <?php if($cooperativeProfile){
+                                                                foreach($cooperativeProfile as $coop){?>
+                                                            <tr>
+                                                                <td><input type="checkbox"  name="checkbox[]" onchange="addToHidden(this)" value="<?php echo $coop['idAccounts'];?>"></td>
+                                                                 <td><?php echo $coop['Cooperative_Name'];?></td>
+                                                                 <td><?php echo $coop['Email_Address'];?></td>
+                                                                 <td>Cooperative</td>
+                                                            </tr>
+                                                            <?php }}?>
+                                                            <?php if($departmentProfile){
+                                                                foreach($departmentProfile as $dept){?>
+                                                            <tr>
+                                                                <td><input type="checkbox" name="checkbox[]" value="<?php echo $dept['idAccounts'];?>" onchange="addToHidden(this)"></td>
+                                                                 <td><?php echo $dept['Department'];?></td>
+                                                                 <td><?php echo $dept['Email_Address'];?></td>
+                                                                 <td>Department</td>
+                                                            </tr>
+                                                            <?php }}?>
+                                                        </tbody>
+                                                    </table>
                                                 </div>
                                             </div>
                                         </div>
 
-                                        </div>
-                                    </fieldset>
+                                    </div>
+                                </fieldset>
+                            </div>
 
-                                </div>
-
-                                <div class="panel-footer">
-                                    <div class="heading-elements">
-                                        <div class="text-right">
-                                            <input type="button" onclick="confirm()" ID="btnSend" text="Submit" class="btn bg-info" value="Submit" />
-                                        </div>
+                            <div class="panel-footer">
+                                <div class="heading-elements">
+                                    <div class="text-right">
+                                        <input type="button" onclick="confirm()" ID="btnSend" text="Submit" class="btn bg-info" value="Submit" />
                                     </div>
                                 </div>
-
                             </div>
-                        </div>
+                        </form>
                     </div>
-                    <!-- Main Content -->
+
+                    
+
+                    <!-- Content area -->
 
                 </div>
-                <!-- /Page content -->
+                <!-- Main Content -->
 
-            </div>
-            <!-- /Page container -->
         </div>
-    </form>
+        <!-- /Page content -->
+
+    </div>
+    <!-- /Page container -->
+                    
 </body>
 </html>
 <script type="text/javascript">
