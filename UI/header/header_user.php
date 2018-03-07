7baacca0bd8ae98ba30a0baef4779112163bd0e7
Dherp Cuevas <34502161+dherpcuevas@users.noreply.github.com>
2018-03-07 22:19:49 +0800

    Add Document

--- UI/header/header_user.php
+++ UI/header/header_user.php
@@ -5,7 +5,7 @@ $arrs = array();
 $title ="";
 $_SESSION['counter']=0;
 $id = $_SESSION['idAccount'];
-if(strpos($_SERVER['REQUEST_URI'],'COOP_AddDocument.php')){
+if(strpos($_SERVER['REQUEST_URI'],'COOP_AddDocument1.php')){
     $arrs[0]="active";
     $arrs[1]="";
     $arrs[2]="";
@@ -230,7 +230,7 @@ else if (strpos($_SERVER['REQUEST_URI'],'EditAccount.php')) {
                                <li>
                                     <a href="#"><i class="icon-file-text2"></i><span> Document</span></a>
                                     <ul>
-                                        <li class="<?php echo $arrs[0]?>"><a href="COOP_AddDocument.php">Send Document</a></li>
+                                        <li class="<?php echo $arrs[0]?>"><a href="COOP_AddDocument1.php">Send Document</a></li>
                                         <li class="<?php echo $arrs[1]?>"><a href="COOP_DocumentList.php">Documents List</a></li>
                                     </ul>
                                 </li>
